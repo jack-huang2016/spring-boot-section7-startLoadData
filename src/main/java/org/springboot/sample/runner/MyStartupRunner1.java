0aef3c15d2923baac2aabd9ff99ff4ebb01456f1
jack-huang2016 <2456120975@qq.com>
2019-01-16 15:21:21 +0800

    第七章节 Spring Boot 启动加载数据 CommandLineRunner 的demo

---
+++ src/main/java/org/springboot/sample/runner/MyStartupRunner1.java
@@ -0,0 +1,15 @@
+package org.springboot.sample.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(value=1)
+public class MyStartupRunner1 implements CommandLineRunner {
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println(">>>>>>>>>>>>>>>服务启动执行，执行加载数据等操作 22222222 <<<<<<<<<<<<<");
+	}
+}
